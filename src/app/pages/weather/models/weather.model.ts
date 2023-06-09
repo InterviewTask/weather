e7e0893c598491c3037cceabad2312e9d00cc2a8
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-06-10 01:31:12 +0330

    fix: weather api interface

--- src/app/pages/weather/models/weather.model.ts
+++ src/app/pages/weather/models/weather.model.ts
@@ -1,3 +1,56 @@
 export interface Weather {
-
+	coord: Coord;
+	weather: IWeather[];
+	base: string;
+	main: Main;
+	visibility: number;
+	wind: Wind;
+	clouds: Cloud;
+	dt: number;
+	sys: Sy;
+	timezone: number;
+	id: number;
+	name: string;
+	cod: number;
 }
+export interface Coord {
+	lon: number;
+	lat: number;
+}
+
+export interface IWeather {
+	id: number;
+	main: string;
+	description: string;
+	icon: string;
+}
+
+export interface Main {
+	temp: number;
+	feels_like: number;
+	temp_min: number;
+	temp_max: number;
+	pressure: number;
+	humidity: number;
+	sea_level: number;
+	grnd_level: number;
+}
+
+export interface Wind {
+	speed: number;
+	deg: number;
+	gust: number;
+}
+
+export interface Cloud {
+	all: number;
+}
+
+export interface Sy {
+	type: number;
+	id: number;
+	country: string;
+	sunrise: number;
+	sunset: number;
+}
+
