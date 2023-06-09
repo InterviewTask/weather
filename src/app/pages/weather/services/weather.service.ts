3ed536239b3a0c0cdb0433da147d9575b1a9ff87
Ali Hakimi <ali.hakimi.njf@gmail.com>
2023-06-10 00:54:47 +0330

    feat: create weather models

--- src/app/pages/weather/services/weather.service.ts
+++ src/app/pages/weather/services/weather.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { WEATHER_API_KEY } from 'src/app/app.config';
+import { WEATHER_API_KEY } from 'src/app/core/conf/app.config';
+import { Weather } from '../models/weather.model';
 
 @Injectable()
 export class WeatherService {
@@ -10,8 +11,8 @@ export class WeatherService {
     private http:HttpClient
   ) { }
 
-  getWeather(city: string): Observable<weather> {
-    return this.http.get<weather>
+  getWeather(city: string): Observable<Weather> {
+    return this.http.get<Weather>
     (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d0ed5bdf9bdeaf55d8224853725405fd&units=metric`)
   }
 }
